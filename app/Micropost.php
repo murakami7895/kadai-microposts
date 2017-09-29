d6f799f5271c3c5b23f83c4719ababe300071873
murakami <murakami7895@gmail.com>
2017-09-29 13:05:57 +0000

    post corrected

--- app/Micropost.php
+++ app/Micropost.php
@@ -8,14 +8,10 @@ class Micropost extends Model
 {
     protected $fillable = ['content', 'user_id'];
 
+   
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-    
-    public function microposts()
-    {
-        return $this->hasMany(Micropost::class);
     }
     
 }
